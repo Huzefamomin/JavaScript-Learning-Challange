0b3aac0d012319249f5279741d8d953ee9fdccc7
Huzefa Momin <90708510+Huzefamomin@users.noreply.github.com>
2023-09-28 08:39:17 +0000

    Opretion updated

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -4,12 +4,12 @@ let score = "33abc" // it will convert in the number
                     // let score = "33"  is actual value number
 // Nan is also a special type 
 
-// console.log(typeof (score))
-// console.log(typeof score)
+console.log(typeof (score))
+console.log(typeof score)
 
 let valueInNumber = Number(score)
-// console.log(typeof valueInNumber)
-// console.log(valueInNumber);
+console.log(typeof valueInNumber)
+console.log(valueInNumber);
 
 //"33" => 33
 // "33wab" ==> NaN
@@ -20,7 +20,7 @@ let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-// console.log( booleanIsLoggedIn);
+console.log( booleanIsLoggedIn);
 
 
 //  1 ==> true; 0 ==> false
@@ -28,37 +28,37 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "Huzefa" => True
 let someNumber = 33
 
-// let StringNumber = String(someNumber)
-// console.log(StringNumber);
-// console.log(typeof StringNumber);
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
 
 
 // -----------------------------------------------operations------------------------------------------------------------------
 
 let value = 3
 let negValue = -value
-// console.log(negValue);
+console.log(negValue);
 
 // basic opration in JS
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2/2);
-// console.log(2%2);
-// console.log(2**2);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
 
 
 let str1 = "Hello"
 let str2 = " Huzefa"
 
 let str3 = str1+str2
-// console.log(str3);
+console.log(str3);
 
-// console.log("1" + 2);      //12
-// console.log(2 + "1");      //12
-// console.log("1" + 2);      //12
-// console.log("1" + 2 + 2);  //122
-// console.log(2 + 2 + "1" ); //41
+console.log("1" + 2);      //12
+console.log(2 + "1");      //12
+console.log("1" + 2);      //12
+console.log("1" + 2 + 2);  //122
+console.log(2 + 2 + "1" ); //41
 
 //  In JS if in first place its a string then it will simply add string to other
 //  if its a number like in ths  console.log(2 + 2 + "1" ); //41 case it 
