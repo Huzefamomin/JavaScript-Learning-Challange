aa35e403b940ad611679c96e0c4f2c775e6e0b01
Huzefa Momin <90708510+Huzefamomin@users.noreply.github.com>
2023-09-28 17:07:23 +0000

    String Detail

---
+++ 01_Basic/07_strings.js
@@ -0,0 +1,199 @@
+
+const name = "Huzefa";
+const repoCount = 50;
+
+// console.log(name+repoCount+" Value");  
+
+console.log("Hello my name is ${name} and my repo count is ${repoCount}");
+
+//It is called string interpolation
+
+
+//String Declatration
+const gameName = new String("Huzefa-HC")
+console.log(gameName);
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2))
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8,4)
+console.log(anotherString);
+
+const newString1 = "  Huzefa  "
+console.log(newString1);
+console.log(newString1.trim());
+
+
+const url = "https://huzefa.com/huzefa%20momin"
+
+console.log(url.replace('%20', '-'));
+console.log(url.includes('huzefa'));
+
+console.log(gameName.split(';'));
+
+// Methods of String 
+// : 
+// ƒ anchor()
+// at
+// : 
+// ƒ at()
+// big
+// : 
+// ƒ big()
+// blink
+// : 
+// ƒ blink()
+// bold
+// : 
+// ƒ bold()
+// charAt
+// : 
+// ƒ charAt()
+// charCodeAt
+// : 
+// ƒ charCodeAt()
+// codePointAt
+// : 
+// ƒ codePointAt()
+// concat
+// : 
+// ƒ concat()
+// constructor
+// : 
+// ƒ String()
+// endsWith
+// : 
+// ƒ endsWith()
+// fixed
+// : 
+// ƒ fixed()
+// fontcolor
+// : 
+// ƒ fontcolor()
+// fontsize
+// : 
+// ƒ fontsize()
+// includes
+// : 
+// ƒ includes()
+// indexOf
+// : 
+// ƒ indexOf()
+// isWellFormed
+// : 
+// ƒ isWellFormed()
+// italics
+// : 
+// ƒ italics()
+// lastIndexOf
+// : 
+// ƒ lastIndexOf()
+// length
+// : 
+// 0
+// link
+// : 
+// ƒ link()
+// localeCompare
+// : 
+// ƒ localeCompare()
+// match
+// : 
+// ƒ match()
+// matchAll
+// : 
+// ƒ matchAll()
+// normalize
+// : 
+// ƒ normalize()
+// padEnd
+// : 
+// ƒ padEnd()
+// padStart
+// : 
+// ƒ padStart()
+// repeat
+// : 
+// ƒ repeat()
+// replace
+// : 
+// ƒ replace()
+// replaceAll
+// : 
+// ƒ replaceAll()
+// search
+// : 
+// ƒ search()
+// slice
+// : 
+// ƒ slice()
+// small
+// : 
+// ƒ small()
+// split
+// : 
+// ƒ split()
+// startsWith
+// : 
+// ƒ startsWith()
+// strike
+// : 
+// ƒ strike()
+// sub
+// : 
+// ƒ sub()
+// substr
+// : 
+// ƒ substr()
+// substring
+// : 
+// ƒ substring()
+// sup
+// : 
+// ƒ sup()
+// toLocaleLowerCase
+// : 
+// ƒ toLocaleLowerCase()
+// toLocaleUpperCase
+// : 
+// ƒ toLocaleUpperCase()
+// toLowerCase
+// : 
+// ƒ toLowerCase()
+// toString
+// : 
+// ƒ toString()
+// toUpperCase
+// : 
+// ƒ toUpperCase()
+// toWellFormed
+// : 
+// ƒ toWellFormed()
+// trim
+// : 
+// ƒ trim()
+// trimEnd
+// : 
+// ƒ trimEnd()
+// trimLeft
+// : 
+// ƒ trimStart()
+// trimRight
+// : 
+// ƒ trimEnd()
+// trimStart
+// : 
+// ƒ trimStart()
+// valueOf
+// : 
+// ƒ valueOf()
+// Symbol(Symbol.iterator)
+// : 
+// ƒ [Symbol.iterator]()
