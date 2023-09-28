91d5cfb9bc667a7ec9eebddb0f379a11848e3e49
Huzefa Momin <90708510+Huzefamomin@users.noreply.github.com>
2023-09-28 08:36:18 +0000

    Opretions Done

--- 01_Basic/03_conversionOperation.js
+++ 01_Basic/03_conversionOperation.js
@@ -8,8 +8,8 @@ let score = "33abc" // it will convert in the number
 // console.log(typeof score)
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
 
 //"33" => 33
 // "33wab" ==> NaN
@@ -20,7 +20,7 @@ let isLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log( booleanIsLoggedIn);
+// console.log( booleanIsLoggedIn);
 
 
 //  1 ==> true; 0 ==> false
@@ -28,6 +28,56 @@ console.log( booleanIsLoggedIn);
 // "Huzefa" => True
 let someNumber = 33
 
-let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// let StringNumber = String(someNumber)
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+// -----------------------------------------------operations------------------------------------------------------------------
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// basic opration in JS
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+
+let str1 = "Hello"
+let str2 = " Huzefa"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);      //12
+// console.log(2 + "1");      //12
+// console.log("1" + 2);      //12
+// console.log("1" + 2 + 2);  //122
+// console.log(2 + 2 + "1" ); //41
+
+//  In JS if in first place its a string then it will simply add string to other
+//  if its a number like in ths  console.log(2 + 2 + "1" ); //41 case it 
+// first add the two numbers and then add the string to number
+
+console.log(true);
+console.log(+true);
+// console.log(true+); it will show error 
+
+console.log(+""); //0 but not preffered
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter=100
+gameCounter ++
+console.log(gameCounter);
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+// link for increment and decrement in JS
+
